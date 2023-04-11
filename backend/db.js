1f22753d647f654365e6f1e8a3a40355f3f15e63
Dheerajsingh002 <dheerajsingh8357@gmail.com>
2023-04-11 19:02:40 +0530

    initial commit

---
+++ backend/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://Dheeraj123:mern123@ac-vmo5jca-shard-00-00.wiuzfps.mongodb.net:27017,ac-vmo5jca-shard-00-01.wiuzfps.mongodb.net:27017,ac-vmo5jca-shard-00-02.wiuzfps.mongodb.net:27017/foodDelivery?replicaSet=atlas-wnl2xy-shard-0&ssl=true&authSource=admin";
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+
+    const foodData = await mongoose.connection.db.collection("food").find({}).toArray();
+    const foodCategoryData = await mongoose.connection.db.collection("foodCategory").find({}).toArray();
+
+    global.food = foodData;
+    global.foodCategory = foodCategoryData;
+
+    console.log(global.food);
+    console.log(global.foodCategory);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = mongoDB;
---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const app=express();
+app.use(express.json())
+const port=process.env.PORT || 5000;
+const mongoDB=require("./db.js")
+mongoDB();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  
+app.get('/',(req,res)=>
+{ 
+      res.send("Hello World ---")
+
+})
+
+app.use('/api',require('./Routes/CreateUser.js'))
+app.use('/api',require('./Routes/DisplayData.js'))
+
+app.listen(port,()=>
+{
+    console.log(`Example app listening on port ${port}`);
+})
